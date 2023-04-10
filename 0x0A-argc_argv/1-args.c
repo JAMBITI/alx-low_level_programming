777f1c9d38dd80e0494e64263c18f47d6cb5e41d
JAMBITI <biti.jamal@gmail.com>
2023-04-10 11:30:43 -0700

    1args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
  * @argv: array of arg
  * Return: always 0
  */
-int main(int argc, char *argv[] __attribute__(unused)))
+int main(int argc, char *argv[])
 {
+	(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
