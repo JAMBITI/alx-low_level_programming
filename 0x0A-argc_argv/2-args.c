8a7ddbeba8d590c67084aff63977f0d8c43e435e
JAMBITI <biti.jamal@gmail.com>
2023-04-10 11:39:30 -0700

    2args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all arguments it receives
+ * @argc: nmbr of arg
+ * @argv: array of arg
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
