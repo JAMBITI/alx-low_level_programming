6a8db5a6fb8819583bd87e2191d7c0878a1c9ad8
JAMBITI <biti.jamal@gmail.com>
2023-04-10 14:25:19 -0700

    mutiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - [rint multiplication of 2 integers
+ * @argc: count arg
+ * @argv: vect arg
+ * Return: 0 if true and 1 if false
+ */
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc == 1)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
