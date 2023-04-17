4b11b6c1ea59b21ad7141b7b32c52ba4f3395bf1
JAMBITI <biti.jamal@gmail.com>
2023-04-17 15:43:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog info
+ * @name: 1er info
+ * @age: second info
+ * @owner: third info
+ *
+ * Description: description
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
