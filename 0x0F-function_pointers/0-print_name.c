ad06c293b45ca304433e50068b52be4cbaf52dc8
JAMBITI <biti.jamal@gmail.com>
2023-04-19 18:38:45 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "function_pointers.h"
+
+/**
+ * print_name - func prints a name
+ * @name: input name
+ * @f: function of name
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif
