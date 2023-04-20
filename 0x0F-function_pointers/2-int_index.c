a2cf7a57a5c6229979bb3fc7005799f249060f6a
JAMBITI <biti.jamal@gmail.com>
2023-04-19 19:36:45 -0700

    tas3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,19 +12,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int a;
+	int a = 0;
 
-	if (array == NULL)
-		return (-1);
-	if (size <= 0)
-		return (-1);
-	if (cmp == NULL)
-		return (-1);
-
-	for (a = 0; a < size; a++)
+	if (array && size && cmp)
+		while (a < size)
 	{
-		if (cmp(array[a]) != 0)
+		if (cmp(array[a]))
 			return (a);
+		a++;
 	}
 	return (-1);
 }
