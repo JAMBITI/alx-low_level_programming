0d9f75e8238a7de0193550f05afc27e8ca29258d
JAMBITI <biti.jamal@gmail.com>
2023-04-20 18:50:05 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * print_strings - Entry Point
+ * @separator: comma space
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i = xn;
+	char *s;
+	va_list valist;
+
+	if (!x)
+		printf("\n");
+	va_start(valist, x);
+	while (i--)
+		printf("%s%s", (s = va_arg(valist, char *)) ? s : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+	va_end(valist);
+}
