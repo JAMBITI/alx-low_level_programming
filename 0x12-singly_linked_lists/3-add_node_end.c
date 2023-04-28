43e440d2754d3f89f893a876c83a234f1a91f032
JAMAL BITI <biti.jamal@gmail.com>
2023-04-28 12:20:55 -0700

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - adds a node to the end of the list
+ * @head: address of pointer to head node 
+ * @str: str field of node 
+ *
+ * Retirn: size of list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+	if(!new_node)
+		return (NULL);
+
+	new_node->str =strdup(str);
+	new_node->len = len;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
+}
