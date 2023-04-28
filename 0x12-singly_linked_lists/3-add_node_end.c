b384126a723af32d021a675e565ee93462674a76
JAMAL BITI <biti.jamal@gmail.com>
2023-04-28 12:32:49 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,10 +4,10 @@
 
 /**
  * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node 
- * @str: str field of node 
+ * @head: address of pointer to head node
+ * @str: str field of node
  *
- * Retirn: size of list
+ * Return: size of list
  */
 
 list_t *add_node_end(list_t **head, const char *str)
@@ -19,10 +19,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if(!new_node)
+	if (!new_node)
 		return (NULL);
 
-	new_node->str =strdup(str);
+	new_node->str = strdup(str);
 	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
