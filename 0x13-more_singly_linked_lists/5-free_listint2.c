50b18c77b4e1f0ed6c96af9bb73b4def39a54f8b
JAMAL BITI <biti.jamal@gmail.com>
2023-05-02 15:54:09 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and set head to null
+ * @head: address of pointer to listing structure
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	temp = *head;
+	while (*head)
+	{
+		temp = *temp;
+		(*head) = (*head)->next;
+		free(temp);
+
+	}
+	*head = NULL;
+}
