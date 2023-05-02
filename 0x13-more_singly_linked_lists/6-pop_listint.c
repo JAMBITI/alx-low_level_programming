d1e97cb3cd2a886ab9568db02f0de2c07ce6a17e
JAMAL BITI <biti.jamal@gmail.com>
2023-05-02 16:04:33 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#inclue "lists.h"
+
+/**
+ * pop_listint - A fonction deletes head node of a list
+ * @head: pointer to listint t structure
+ * Return: value of popped node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
