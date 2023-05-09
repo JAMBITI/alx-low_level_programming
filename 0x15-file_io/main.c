fc0e2f0c452e2aa7501a3b6cb2939d245f901bc7
JAMAL BITI <biti.jamal@gmail.com>
2023-05-09 07:07:41 -0700

    ok

--- 0x15-file_io/main.c
+++ 0x15-file_io/main.c
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define READ_BUF_SIZE 1024
 
