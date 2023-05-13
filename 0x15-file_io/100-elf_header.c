7727fcdda62d3a0f8fad4be10c97f73a7e2da1c7
JAMAL BITI <biti.jamal@gmail.com>
2023-05-13 06:06:14 -0700

    task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for program to get header of ELF
+ * @argc: number of arg
+ * @argv: pointer to array of arg
+ *
+ * Return: 1 on success, error code on failure
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+
+	return (1);
+
+}
