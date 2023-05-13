b68161c904a6f4f7634503ce1b379c6668861258
JAMAL BITI <biti.jamal@gmail.com>
2023-05-13 05:41:33 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file end
+ * @filename: name of file to have to create
+ * @text_content: string  to write at the end of file
+ *
+ * Return: 1 on success 0 on failure
+ *
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op;
+	int wr;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+
+}
