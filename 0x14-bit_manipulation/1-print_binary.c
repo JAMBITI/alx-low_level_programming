691d8ce9fd6601bcc4ccf6bfcb9932929d490479
JAMBITI <biti.jamal@gmail.com>
2023-07-12 11:12:51 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,26 +8,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1L << (sizeof(n) * 8 - 1);
-	int printed = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (mask != 0)
-	{
-		if (n & mask)
-		{
-			_putchar('1');
-			printed++;
-		}
-		else if (printed)
-		{
-			_putchar('0');
-		}
-		mask >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
